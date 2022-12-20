35686f1a4ca5bacc796d1dc5fd10c77dc1a69990
jasiel <jasiel_serra@hotmail.com>
2022-12-20 16:35:56 -0300

    Using router link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
-
+    <div id="nav">
+      <router-link to="/">
+        <img src="" alt="" id="logo">
+      </router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "Navbar.vue"
+  name: "Navbar"
 }
 </script>
 
