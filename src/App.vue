6f496d95d1fa1ada5ec406b2a0475e913aa65e56
jasiel <jasiel_serra@hotmail.com>
2022-12-20 16:39:27 -0300

    Import Footer at App View

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <Navbar/>
   <router-view/>
+  <Footer />
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import Footer from "./components/Footer.vue"
 
 export default {
-  components: {Navbar}
+  components: {
+    Navbar,
+    Footer,
+  }
 }
 </script>
 
