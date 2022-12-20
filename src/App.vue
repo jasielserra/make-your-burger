e216acde8bca024707765b65125744a321ba85e8
jasiel <jasiel_serra@hotmail.com>
2022-12-20 20:03:04 -0300

    Test Pushing

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 import Navbar from "@/components/Navbar";
-import Footer from "./components/Footer.vue"
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
