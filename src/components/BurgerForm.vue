883bcb18e81a2ada90fb3084fe7d850f2e551065
jasiel <jasiel_serra@hotmail.com>
2022-12-21 11:08:00 -0300

    Fetch opcional on Json Data Base Server.

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -18,22 +18,14 @@
           <label for="carne">Escolha a carne do seu Burger:</label>
           <select name="carne" id="carne" v-model="carne">
             <option value="">Selecione o tipo de carne</option>
-            <option value="maminha">Maminha</option>
+            <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
           </select>
         </div>
         <div id="opcionais-container" class="input-container">
           <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
-          <div class="checkbox-container">
-            <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-            <span>Salame</span>
-          </div>
-          <div class="checkbox-container">
-            <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-            <span>Salame</span>
-          </div>
-          <div class="checkbox-container">
-            <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-            <span>Salame</span>
+          <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+             <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+             <span>{{ opcional.tipo }}</span>
           </div>
         </div>
         <div class="input-container">
