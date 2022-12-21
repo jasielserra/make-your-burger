3e0d626cbf53531b9d27658c818762c8c5de9144
jasiel <jasiel_serra@hotmail.com>
2022-12-21 10:33:24 -0300

    Create Data for fill form.

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -14,22 +14,30 @@
             <option value="integral">Integral</option>
           </select>
         </div>
-          <div class="input-container">
+        <div class="input-container">
           <label for="carne">Escolha a carne do seu Burger:</label>
           <select name="carne" id="carne" v-model="carne">
             <option value="">Selecione o tipo de carne</option>
             <option value="maminha">Maminha</option>
           </select>
-          </div>
-          <div class="input-container">
-          <label for="opcionais">Escolha os opcionais:</label>
+        </div>
+        <div id="opcionais-container" class="input-container">
+          <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
           <div class="checkbox-container">
             <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
             <span>Salame</span>
-        </div>
+          </div>
+          <div class="checkbox-container">
+            <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+            <span>Salame</span>
+          </div>
+          <div class="checkbox-container">
+            <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+            <span>Salame</span>
+          </div>
         </div>
         <div class="input-container">
-         <input type="submit" class="submit-btn" value="Criar meu Burger!">
+          <input type="submit" class="submit-btn" value="Criar meu Burger!">
         </div>
       </form>
 
@@ -40,10 +48,89 @@
 
 <script>
 export default {
-  name: "BurgerForm"
+  name: "BurgerForm",
+  data(){
+    return {
+      paes: null,
+      carnes: null,
+      opcionaisdata: null,
+      nome: null,
+      pao: null,
+      carne: null,
+      opcionais: [],
+      status: "Solicitado",
+      msg: null,
+    }
+  }
+
 }
 </script>
 
 <style scoped>
+
+  #burger-form {
+    max-width: 400px;
+    margin: 0 auto;
+  }
+
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+
+  label {
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;
+    padding: 5px 10px;
+    border-left: 4px solid #FCBA03;
+  }
+
+  input, select {
+    padding: 5px 10px;
+    width: 300px;
+  }
+
+  #opcionais-container {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .checkbox-container {
+    display: flex;
+    align-items: flex-start;
+    width: 50%;
+    margin-bottom: 20px;
+  }
+
+  .checkbox-container span,
+  .checkbox-container input {
+    width: auto;
+  }
+
+  .checkbox-container span {
+    margin-left: 6px;
+    font-weight: bold;
+  }
+
+  .submit-btn {
+    background-color: #222;
+    color: #FCBA03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+  }
+
+  .submit-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
+
+
 
 </style>
