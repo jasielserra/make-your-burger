d39eaea3554c0accf452eb73fbbf073fe9f66e3e
jasiel <jasiel_serra@hotmail.com>
2022-12-22 10:46:36 -0300

    Mensagens nas atualizações e remoções dos pedidos.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="burger-table">
+    <Message :msg="msg" v-show="msg" />
     <div>
       <div id="burger-table-heading">
         <div class="order-id">#:</div>
@@ -11,53 +12,113 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>
           </ul>
+        </div>
+
+        <div>
+          <select name="status" class="status" @change="updatedBurger($event, burger.id)">
+            <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
+              {{ s.tipo }}
+            </option>
+          </select>
+         <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
       </div>
 
-       <div>
-         <select name="status" class="status">
-           <option value="">Selecione:</option>
-         </select>
-         <button class="delete-btn">Cancelar</button>
-      </div>
-
-      <div class="burger-table-row">
-        <div class="order-number">2</div>
-        <div>João</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
-        <div>
-          <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
-          </ul>
-        </div>
-      </div>
-
-      <div>
-         <select name="status" class="status">
-           <option value="">Selecione:</option>
-         </select>
-         <button class="delete-btn">Cancelar</button>
-      </div>
     </div>
   </div>
 
 </template>
 
 <script>
+import Message from "@/components/Message";
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  components: {Message},
+  data(){
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+      msg: null
+    }
+  },
+  methods: {
+    async getPedidos(){
+
+      const req = await fetch("http://localhost:3000/burgers");
+
+      const data = await req.json();
+
+      this.burgers = data;
+
+      console.log(this.burgers);
+      // resgatar os status
+      this.getStatus();
+
+    },
+    async getStatus(){
+
+      const req = await fetch("http://localhost:3000/status");
+
+      const data = await req.json();
+
+      this.status = data;
+
+      console.log(data);
+
+    },
+    async deleteBurger(id){
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE"
+      });
+
+      const res = await req.json();
+
+       // colocar uma msg de sistema
+      this.msg = `Pedido removido com sucesso`;
+
+      // limpar msg
+      setTimeout(() => this.msg = "", 3000);
+
+
+      // msg
+      this.getPedidos();
+
+    },
+    async updatedBurger(event, id){
+      const option = event.target.value;
+
+      const dataJson = JSON.stringify({status:option});
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {method: "PATCH",
+      headers: {"Content-Type": "application/json"},
+      body: dataJson
+    })
+
+     const res = await req.json();
+
+
+       // colocar uma msg de sistema
+     this.msg = `Pedido Nº ${res.id} foi atualizado para ${res.status}!`;
+
+      // limpar msg
+     setTimeout(() => this.msg = "", 3000);
+
+     console.log(res)
+
+    }
+  },
+  mounted() {
+    this.getPedidos();
+  }
 }
 </script>
 
